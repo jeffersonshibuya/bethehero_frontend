d0e7e60c84c42c9978398d5760a326a9e763d06c
Jefferson Shibuya <jeffersonshibuya@yahoo.com.br>
2020-04-27 13:51:09 -0300

    Set heroku server backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  // baseURL: "http://localhost:3000",
+  baseURL: "https://be-the-hero-ong.herokuapp.com/",
 });
 
 export default api;
